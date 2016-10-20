c35a9baea69fec377066efe5693206c758770f62
laserlens <laserlens@gmail.com>
2016-10-20 14:34:16 -0500

    added click event to random image and styles with ng-class

--- public/scripts/main.controller.js
+++ public/scripts/main.controller.js
@@ -3,11 +3,24 @@ app.controller('MainController', MainController);
 function MainController(giphy) {
   var main = this;
 
+  main.classHolder = 'marginHolder'
+
   console.log('MainController Loaded');
 
-  main.randomGiphy = function() {
+  main.imageButton = function () {
+    console.log('the image is a button');
     giphy.getRandom().then(function(image) {
       main.image = image;
+      window.open(main.image);
+    });
+  };
+
+  main.randomGiphy = function() {
+    main.classHolder = '';
+    main.classRandom = 'random';
+    giphy.getRandom().then(function(image) {
+      main.image = image;
+      main.link = image;
       console.log(main.image);
     });
   };//end of randomGiphy
@@ -15,6 +28,7 @@ function MainController(giphy) {
   main.searchImages = [];
 
   main.searchGiphy = function () {
+    main.classSearch = 'random';
     giphy.getSearch(main.search).then(function (images) {
       main.searchImages = images;
       console.log('search main.image', main.searchImages);
