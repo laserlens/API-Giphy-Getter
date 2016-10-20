4aec7324ee88277bbd2a616f18e27dea7fea9f99
laserlens <laserlens@gmail.com>
2016-10-20 16:23:19 -0500

    created click event on searched images takes u too the image you clicks original url also added hover over affect

--- public/scripts/main.controller.js
+++ public/scripts/main.controller.js
@@ -5,33 +5,36 @@ function MainController(giphy) {
 
   main.classHolder = 'marginHolder'
 
-  console.log('MainController Loaded');
+  //console.log('MainController Loaded');
 
-  main.imageButton = function () {
-    console.log('the image is a button');
-    giphy.getRandom().then(function(image) {
-      main.image = image;
-      window.open(main.image);
-    });
-  };
+  main.image = '';
 
   main.randomGiphy = function() {
     main.classHolder = '';
     main.classRandom = 'random';
     giphy.getRandom().then(function(image) {
       main.image = image;
-      main.link = image;
-      console.log(main.image);
+      //console.log(main.image);
     });
   };//end of randomGiphy
 
+//function on click event of picture takes to orginal url
+  main.imageButton = function () {
+      window.open(main.image);
+  };
+
   main.searchImages = [];
+
+//function on click event of picture takes to orginal url
+  main.searchImagesURL = function (i) {
+    window.open(main.searchImages[i].images.original.url);
+  };
 
   main.searchGiphy = function () {
     main.classSearch = 'random';
     giphy.getSearch(main.search).then(function (images) {
       main.searchImages = images;
-      console.log('search main.image', main.searchImages);
+      //console.log('search main.image', main.searchImages);
     });
   };//end of searchGiphy
 
